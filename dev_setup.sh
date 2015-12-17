905bf792636941385916be90a9e79b2f28c57a37
Katharine Berry <katharine@getpebble.com>
2015-12-17 12:46:57 -0800

    Updated stuff.

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Run the general build.
 docker-compose build
+# Do this in the mounted directory, since the Dockerfile did it in a folder we
+# mask by mounting over it.
 docker-compose run web sh -c "rm -rf bower_components && cd /tmp && python /code/manage.py bower install && mv bower_components /code/"
+# Stop everything again.
 docker-compose stop
